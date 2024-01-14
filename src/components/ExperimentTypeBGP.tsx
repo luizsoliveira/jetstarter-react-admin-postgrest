3fe81cbe4c2c608e939e2e5083e683ca0f5b3e75
Luiz Oliveira <luizsoliveira@gmail.com>
2024-01-13 23:37:19 -0800

    Creating radio button input to map the type of data partitioning reference just anomalous or entire dataset

--- src/components/ExperimentTypeBGP.tsx
+++ src/components/ExperimentTypeBGP.tsx
@@ -4,8 +4,13 @@ import stylesAdmin from '../styles/admin.module.css'
 export const ExperimentTypeBGP = () => (
     <>
         <div className={stylesAdmin.inlineGroupFields}>
-            <NumberInput source="parameters.data_partition_training" validate={[required()]} min={1} max={100} step={1} label="Percentage of data for training (%)" helperText="Percentage of the total data that will be used for training during data partitioning step." />
+            <NumberInput source="parameters.data_partition_training" validate={[required()]} min={1} max={100} step={1} label="Percentage of data for training (%)" helperText="Percentage of the total data (or anomalous data) that will be used for training during data partitioning step." />
         </div>
+        
+        <RadioButtonGroupInput source="parameters.data_partition_reference" defaultValue="anomalous" label="Reference to calculate the partitions" choices={[
+        { id: 'anomalous', name: 'Anomalous data points' },
+        { id: 'total', name: 'Entire dataset' },
+        ]} />
         <SelectInput label="Sequence length RNN" source="parameters.rnn_length" choices={[
         { id: '5', name: '5' },
         { id: '10', name: '10' },
