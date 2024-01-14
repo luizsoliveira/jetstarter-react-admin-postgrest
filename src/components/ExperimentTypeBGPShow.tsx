b30b9f0f18e4d8961b89cd01b6efe1b7c6a17d47
Luiz Oliveira <luizsoliveira@gmail.com>
2024-01-13 20:52:56 -0800

    Component to show Experiments of BGP type

---
+++ src/components/ExperimentTypeBGPShow.tsx
@@ -0,0 +1,19 @@
+import {Labeled, NumberField, NumberInput, RadioButtonGroupInput, SelectInput, required } from "react-admin";
+import stylesAdmin from '../styles/admin.module.css'
+
+export const ExperimentTypeBGPShow = () => (
+    <>
+        <div className={stylesAdmin.inlineGroupFields}>
+            <Labeled label="Percentage of data for training (%)">
+                <NumberField source="parameters.data_partition_training" label="Percentage of data for training (%)" helperText="Percentage of the total data that will be used for training during data partitioning step." />
+            </Labeled>                
+        </div>
+        <Labeled label="Sequence length RNN">
+                <NumberField source="parameters.rnn_length" label="Sequence length RNN" />
+        </Labeled>
+        <Labeled label="Debug">
+                <NumberField source="parameters.debug" label="Debug" />
+        </Labeled>        
+    </>
+                  
+)
