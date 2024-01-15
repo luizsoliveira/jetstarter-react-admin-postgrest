9de66a3b1c71ea1477625de5fed7791629447755
Luiz Oliveira <luizsoliveira@gmail.com>
2024-01-15 15:08:52 -0800

    Including data_partition_reference field

--- src/components/ExperimentTypeBGP.tsx
+++ src/components/ExperimentTypeBGP.tsx
@@ -7,16 +7,16 @@ export const ExperimentTypeBGP = () => (
             <NumberInput source="parameters.data_partition_training" validate={[required()]} min={1} max={100} step={1} label="Percentage of data for training (%)" helperText="Percentage of the total data (or anomalous data) that will be used for training during data partitioning step." />
         </div>
         
-        <RadioButtonGroupInput source="parameters.data_partition_reference" defaultValue="anomalous" label="Reference to calculate the partitions" choices={[
+        <RadioButtonGroupInput source="parameters.data_partition_reference" validate={[required()]} defaultValue="anomalous" label="Reference to calculate the partitions" choices={[
         { id: 'anomalous', name: 'Anomalous data points' },
         { id: 'total', name: 'Entire dataset' },
         ]} />
-        <SelectInput label="Sequence length RNN" source="parameters.rnn_length" choices={[
+        <SelectInput label="Sequence length RNN" validate={[required()]} source="parameters.rnn_length" choices={[
         { id: '5', name: '5' },
         { id: '10', name: '10' },
         { id: '20', name: '20' },
         ]} />
-        <RadioButtonGroupInput source="parameters.debug" defaultValue="disabled" label="Debug" choices={[
+        <RadioButtonGroupInput source="parameters.debug" validate={[required()]} defaultValue="disabled" label="Debug" choices={[
         { id: 'activated', name: 'Activated' },
         { id: 'disabled', name: 'Disabled' },
         ]} />
--- src/components/ExperimentTypeBGPShow.tsx
+++ src/components/ExperimentTypeBGPShow.tsx
@@ -6,14 +6,23 @@ export const ExperimentTypeBGPShow = () => (
         <div className={stylesAdmin.inlineGroupFields}>
             <Labeled label="Percentage of data for training (%)">
                 <NumberField source="parameters.data_partition_training" label="Percentage of data for training (%)" helperText="Percentage of the total data that will be used for training during data partitioning step." />
-            </Labeled>                
+            </Labeled>
         </div>
-        <Labeled label="Sequence length RNN">
-                <NumberField source="parameters.rnn_length" label="Sequence length RNN" />
-        </Labeled>
-        <Labeled label="Debug">
-                <NumberField source="parameters.debug" label="Debug" />
-        </Labeled>        
+        <div className={stylesAdmin.inlineGroupFields}>
+            <Labeled label="Reference to calculate the partitions">
+                    <NumberField source="parameters.data_partition_reference" label="Reference to calculate the partitions" />
+            </Labeled>
+        </div>
+        <div className={stylesAdmin.inlineGroupFields}>
+            <Labeled label="Sequence length RNN">
+                    <NumberField source="parameters.rnn_length" label="Sequence length RNN" />
+            </Labeled>
+        </div>
+        <div className={stylesAdmin.inlineGroupFields}>
+            <Labeled label="Debug">
+                    <NumberField source="parameters.debug" label="Debug" />
+            </Labeled>
+        </div>
     </>
                   
 )
